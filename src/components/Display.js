92316e8e66f1b3dce4e8beb18c61e2ea342a9014
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-07-24 11:48:44 -0700

    nothing much done

---
+++ src/components/Display.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Display(props) {
+    return (
+        <div id='display'></div>
+    );
+}
---
+++ src/components/Key.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Key(props) {
+    return (
+        <div id={props.name} className='key'></div>
+    );
+}

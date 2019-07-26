28fce4fba5fe4aa3700ea212dd1a34509b506d39
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-07-26 14:42:24 -0700

    Change the design

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,8 +3,7 @@ import React from 'react';
 export function Display(props) {
     return (
         <div id='display'>
-            <p id='input'>{props.m_input}</p>
-            <p id='result'>{(props.m_result !== undefined) ? props.m_result : '0'}</p>
+            {(props.m_result !== undefined) ? props.m_result : '0'}
         </div>
     );
 }

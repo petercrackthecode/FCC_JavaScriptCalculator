df8031d2c91bb488f47bfc05864c390e3de57632
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-07-26 15:50:49 -0700

    Just keep going

--- src/App.js
+++ src/App.js
@@ -18,14 +18,11 @@ function App() {
     state.m_operator= undefined;
   }
 
-  const calculateResult= () => {
-    const result= state.m_result;
-    const tempNum= state.m_tempNum;
-    const operator= state.m_operator;
+  const calculateResult= (result, operator, tempNum) => {
     // cleanUp before switch-case because they have return
-    let returnResult= undefined;
+    let returnResult= 0;
 
-    switch(state.m_operator) {
+    switch(operator) {
       case '+': returnResult= result + tempNum;
       break;
       case '-': returnResult= result - tempNum;
@@ -35,12 +32,10 @@ function App() {
       case '/':
         if (tempNum === 0) {
           state.m_output= "Error: Division by zero";
-          returnResult= 0;
         }
-
-        returnResult= result / tempNum;
+        else returnResult= result / tempNum;
       break;
-      default: returnResult= 0;
+      default:
       break;
     }
 
@@ -51,18 +46,24 @@ function App() {
 
   const onFormulaChange= (key) => {
     switch (key) {
-      case '=': {
-        if (state.m_tempNum !== undefined) state.m_result= calculateResult();
-      }
+      case '=':
+        if (state.m_tempNum !== undefined) state.m_result= calculateResult(state.m_result, state.m_operator, state.m_tempNum);
+      break;
       case '+':
       case '-':
       case '*':
-      case '/': {
+      case '/':
         if (state.m_tempNum === undefined) {
           state.m_operator= key;
         }
-        else calculateResult();
-      }
+        else state.m_result= calculateResult(state.m_result, state.m_operator, state.tempNum);
+      break;
+      case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+        state.m_input+= key;
+      break;
+      case '0':
+        if (!(state.m_input[0] === '0' || state.m_input.length === 0)) state.m_input+= key;
+      break;
       default: return;
     }
   };

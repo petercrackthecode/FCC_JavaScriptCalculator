35d31201506f673d20827d3474538df90302ec48
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-08-02 12:18:48 -0700

    change it to a class, encounter some bugs, will fix soon.

--- src/App.js
+++ src/App.js
@@ -4,26 +4,30 @@ import {Display} from './components/Display.js';
 import {KeyPad} from './components/KeyPad.js';
 import './styles/App.css';
 
-function App() {
-  let state= {
-    m_result: 12,
-    // m_formula has a setup order as [firstNum, operator, secondNum]
-    m_formula: [],
-    m_isDivisionByZero: false,
-  };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      m_result: 12,
+      // m_formula has a setup order as [firstNum, operator, secondNum]
+      m_formula: [],
+      m_isDivisionByZero: false,
+    };
+  }
 
-  const emptyFormula= () => {
-    state.m_formula= [];
-  };
 
-  const clearAll= () => {
-    emptyFormula();
-    state.m_result= 0;
-  };
+  emptyFormula= () => {
+    this.setState({m_formula: []});
+  }
+
+  clearAll= () => {
+    this.emptyFormula();
+    this.setState({m_result: 0});
+  }
 
   // you must check for division by zero here
-  const calculateResult= (firstNum, operator, secondNum) => {
-    let result= state.m_result;
+  calculateResult= (firstNum, operator, secondNum) => {
+    let result= this.state.m_result;
 
     switch (operator) {
       case '+': result= firstNum + secondNum;
@@ -33,7 +37,7 @@ function App() {
       case '*': result= firstNum * secondNum;
       break;
       case '/':
-        if (!secondNum) state.m_isDivisionByZero= true;
+        if (!secondNum) this.setState({m_isDivisionByZero: true});
         else result= firstNum / secondNum;
       break;
       default:
@@ -43,49 +47,59 @@ function App() {
     return result;
   };
 
-  const onChange= (key) => {
+  onChange= (key) => {
     console.log("onChange activates on " + key);
-    state.m_result= key;
-    console.log("state.m_result= " + state.m_result);
+    this.setState({m_result: key});
+    console.log("state.m_result= " + this.state.m_result);
   }
 
-  /*
-  const onChange= (key) => {
-    const formulaLength= state.m_formulaLength;
+  onChange= (key) => {
+    const formulaLength= this.state.m_formulaLength;
+
     switch (key) {
       // every time an user presses '=' immediately show the result
       case '=':
         switch (formulaLength) {
           case 3:
-            state.m_result= calculateResult(parseFloat(state.m_formula[0]), state.m_formula[1], parseFloat(state.m_formula[2]));
+            this.setState({
+              m_result: this.calculateResult(parseFloat(this.state.m_formula[0]), this.state.m_formula[1], parseFloat(this.state.m_formula[2]))
+            });
           break;
           case 2: case 1:
-            state.m_result= state.m_formula[0];
+            this.setState({m_result: this.state.m_formula[0]});
           break;
           case 0:
           default:
           break;
         }
-        emptyFormula();
+        this.emptyFormula();
       break;
       case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
         if (formulaLength === 0 || formulaLength === 2)
-          if (!(key === 0 && state.m_formula[formulaLength - 1].length === 0))
-            state.m_formula.push(key);
+          if (!(key === 0 && this.state.m_formula[formulaLength - 1].length === 0)) {
+            this.setState({m_formula: this.state.m_formula.concat(key)});
+          }
         else
-          state.m_formula[formulaLength - 1]+= key;
+          currentFormula[formulaLength - 1]+= key;
+
+        this.setState({m_formula: currentFormula});
       break;
       case '+': case '-': case '*': case '/':
         switch (formulaLength) {
           case 0:
-            state.m_formula.push(state.m_result);
+            currentFormula.push(this.state.m_result);
+            this.setState({m_formula: currnetFormula});
           break;
           case 1: case 2:
           break;
           case 3:
-            state.m_result= calculateResult(Number(state.m_formula[0]), state.m_formula[1], Number(state.m_formula[2]));
-            emptyFormula();
-            state.m_formula.push(state.m_result);
+            this.setState({
+              m_result: this.calculateResult(Number(this.state.m_formula[0]), this.state.m_formula[1], Number(this.state.m_formula[2]))
+            });
+            this.emptyFormula();
+            currentFormula= [];
+            currentFormula.push(this.state.m_result);
+            this.setState({m_formula: currentFormula});
           break;
           default:
           break;
@@ -93,22 +107,23 @@ function App() {
         state.m_formula.push(key);
       break;
       case 'AC':
-        clearAll();
+        this.clearAll();
       break;
       default:
       break;
     }
-  };
-  */
+  }
 
-  return (
-    <div id="app">
-      <div id='calculator'>
-        <Display m_result={state.m_result}/>
-        <KeyPad onChange={onChange}/>
+  render() {
+    return (
+      <div id="app">
+        <div id='calculator'>
+          <Display m_result={this.state.m_result}/>
+          <KeyPad onChange={this.onChange}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
